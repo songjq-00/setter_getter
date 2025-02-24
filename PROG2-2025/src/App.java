e764feb740a33119bda6178a483590e84cb5c390
songjq-00 <2496180611@qq.com>
2025-02-24 18:42:55 +0800

    commit

--- PROG2-2025/src/App.java
+++ PROG2-2025/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        SliceOHeaven luckcoffee = new SliceOHeaven();
-        luckcoffee.takeOrder("1234",  20.00);
+        SliceOHeaven sliceoheaven = new SliceOHeaven();
+        sliceoheaven.takeOrder("1234",  20.00);
         
     }
 }
